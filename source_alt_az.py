b95ad5624ca3c043c5a8df332ea221297c82faa5
Vivek Gupta <vgupta@mpsr-srv0.obs.molonglo.local>
2020-07-21 22:56:03 +1000

    Added feature to predict rise and set times

--- source_alt_az.py
+++ source_alt_az.py
@@ -1,6 +1,6 @@
 from __future__ import division, print_function
 import argparse, sys
-
+import numpy as np
 from astropy.coordinates import AltAz, Angle, EarthLocation, SkyCoord, FK5
 from astropy.time import Time
 from astropy import units as u
@@ -38,13 +38,56 @@ def main():
 
   obs_time = Time(args.obs_time) 
 
+  #Get the alt_az values for the whole LST day, centered around obs_time
+  seconds_per_sidereal_day = 86164.09
+  times = obs_time + (np.arange(0, seconds_per_sidereal_day, args.precision) - seconds_per_sidereal_day/2) * u.second
+  
   source_altaz = source.transform_to(AltAz(location=obs_loc, obstime=obs_time))
+  all_alt_az = source.transform_to(AltAz(location=obs_loc, obstime=times)) 
   
-  visible=False
-  if source_altaz.alt.value > ob[3]:
-    visible=True
-  
+  #Find the times when the source rises and sets
+
+  signs = np.sign(all_alt_az.alt.value - ob[3])
+  diffs = np.diff(signs)
+
+  max_alt_idx = np.argmax(all_alt_az.alt.value)
+  max_alt = "{0:.1f} degrees".format(all_alt_az.alt.value[max_alt_idx])
+  max_alt_time = str(times[max_alt_idx])
+
+  if not np.any(diffs):
+    #All values inside signs must be same, therefore, the source never crossed the horizon
+
+    if np.all(signs == 1):
+      visible = True
+      rise_time = "Always up"
+      set_time = "Always up"
+    
+    elif np.all(signs == -1):
+      visible = False
+      rise_time = "Never up"
+      set_time = "Never up"
+
+  else:
+    #Source would have crossed the horizon twice. The diffs array would contain all 0s except for two places which should have +2 and -2.
+    rise_idx = np.where(np.diff(np.sign(all_alt_az.alt.value - ob[3])) > 0)[0][0]
+    set_idx = np.where(np.diff(np.sign(all_alt_az.alt.value - ob[3])) < 0)[0][0]
+
+    rise_time = str(times[rise_idx])
+    set_time = str(times[set_idx])
+
+    visible=False
+    if source_altaz.alt.value > ob[3]:
+      visible=True
+
+  print("At the requested obs_time(UTC) - {0}:".format(obs_time))
   print("Source visible : {0}".format(visible))
+  print("Source altitude : {0}".format(source_altaz.alt))
+  print("Source azimuth : {0}".format(source_altaz.az))
+
+  print("\n")
+  print("Nearest rise time(UTC): {0}".format(rise_time))
+  print("Nearest set time(UTC): {0}".format(set_time))
+  print("Max_altitide acheived: {0} at UTC: {1}".format(max_alt, max_alt_time))
 
 if __name__ == '__main__':
   for i, arg in enumerate(sys.argv):
@@ -57,6 +100,7 @@ if __name__ == '__main__':
       Resolvable by astropy (uses SIMBAD)")
   a.add_argument("-source_coords", type=str, nargs=2, help="RA (hourangle) and Dec (degree) of the source")
   a.add_argument("-obs_time", type=str, help="Time of observation in UTC (fmt=YYYY-mm-ddTHH:MM:SS). Def = Time.now()", default = str(Time.now()))
+  a.add_argument("-precision", type=float, help="Precision (in seconds) when computing the rise and set times (def = 60 secs)", default=60)
   args = a.parse_args()
   main()
 
